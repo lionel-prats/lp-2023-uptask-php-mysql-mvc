cf5649d644c1e547ffaac70fe62ff7a856851f1f
lionel-prats <lionel.prats.c@gmail.com>
2023-06-22 01:33:05 -0300

    seccion 60 - video 564

--- Router.php
+++ Router.php
@@ -19,8 +19,8 @@ class Router
 
     public function comprobarRutas()
     {
-
-        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        // $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $currentUrl = $_SERVER['REQUEST_URI'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
