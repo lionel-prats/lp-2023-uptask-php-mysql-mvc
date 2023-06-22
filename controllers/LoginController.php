0cbec2eddccd771ce509a49b99956a7ffb6a7ee2
lionel-prats <lionel.prats.c@gmail.com>
2023-06-22 01:45:45 -0300

    seccion 61 - video 565

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,24 +2,30 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController {
     
-    public static function login(){
-        echo "Desde Login";
+    public static function login(Router $router){
         if($_SERVER["REQUEST_METHOD"]){
 
         }
+        $router->render("auth/login", [
+            'titulo' => 'Iniciar Sesión'
+        ]);
     }
     
     public static function logout(){
         echo "Desde Logout";
     }
     
-    public static function crear(){
-        echo "Desde Crear";
+    public static function crear(Router $router){
         if($_SERVER["REQUEST_METHOD"]){
 
         }
+        $router->render("auth/crear", [
+            'titulo' => 'Crear Cuenta'
+        ]);
     }
     
     public static function olvide(){
