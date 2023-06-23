64a30b7c6772bd465979eba2574980d8c6c380bb
lionel-prats <lionel.prats.c@gmail.com>
2023-06-23 00:26:21 -0300

    seccion 63 - video 578

---
+++ models/Usuario.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Model;
+
+class Usuario extends ActiveRecord {
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id','nombre','apellido','email','password','telefono','admin','confirmado','token'];
+
+    public $id;
+    public $nombre;
+    public $email;
+    public $password;
+    public $token;
+    public $confirmado;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->token = $args['token'] ?? '';
+        $this->confirmado = $args['confirmado'] ?? '0';
+    }
+
+
+}
