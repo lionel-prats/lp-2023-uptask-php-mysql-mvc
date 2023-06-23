03f0bf2906cda0520a8429008132c0f0b733fb75
lionel-prats <lionel.prats.c@gmail.com>
2023-06-22 23:04:12 -0300

    seccion 61 - video 570

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,32 +1,33 @@
-<div class="contenedor crear">
+<div class="contenedor login">
     <h1 class="uptask">UPTask</h1>
     <p class="tagLine">Crea y Administra tus Proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+        <form class="formulario" method="POST">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input 
+                    type="email"
+                    id="email"
+                    placeholder="Tu Email"
+                    name="email"
+                >
+            </div>
+            <div class="campo">
+                <label for="password">Password</label>
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Tu Password"
+                    name="password"
+                >
+            </div>
+            <input type="submit" value="Iniciar Sesión" class="boton">
+        </form>
+        <div class="acciones">
+            <a href="/crear">Crear Cuenta</a>
+            <a href="/olvide">Olvidé mi Password</a>
+        </div>
+    </div><!-- .contenedor-sm -->
 </div>
-<div class="contenedor-sm">
-    <p class="descripcion-pagina">Iniciar Sesión</p>
-    <form class="formulario" method="POST">
-        <div class="campo">
-            <label for="email">Email</label>
-            <input 
-                type="email"
-                id="email"
-                placeholder="Tu Email"
-                name="email"
-            >
-        </div>
-        <div class="campo">
-            <label for="password">Password</label>
-            <input 
-                type="password"
-                id="password"
-                placeholder="Tu Password"
-                name="password"
-            >
-        </div>
-        <input type="submit" value="Iniciar Sesión" class="boton">
-    </form>
-    <div class="acciones">
-        <a href="/crear">Crear Cuenta</a>
-        <a href="/olvide">Olvidé mi Password</a>
-    </div>
-</div><!-- .contenedor-sm -->
