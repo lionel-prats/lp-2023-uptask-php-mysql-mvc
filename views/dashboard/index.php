3429823b825834766b1bbb3f1343e3e54cebd328
lionel-prats <lionel.prats.c@gmail.com>
2023-06-27 00:09:48 -0300

    seccion 68 - video 612

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,5 +1,18 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
-
+<?php if(!count($proyectos)): ?>
+    <p class="no-proyectos">
+        No Hay Proyectos Aún 
+        <a href="/crear-proyecto">Comienza creando uno</a>
+    </p>
+<?php else: ?>
+    <ul class="listado-proyectos">
+        <?php foreach($proyectos as $proyecto): ?>
+            <li class="proyecto">
+                <a href="/proyecto?id=<?= $proyecto->url; ?>"><?= $proyecto->proyecto; ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
