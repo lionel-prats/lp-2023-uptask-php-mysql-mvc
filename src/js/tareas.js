18fb25a27c63e73e7758eb6faec1d34fc91d791d
lionel-prats <lionel.prats.c@gmail.com>
2023-06-27 01:23:11 -0300

    seccion 69 - video 616

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -1,10 +1,35 @@
 // IIFE -> Inmediately Invoke Function Expression (VIDEO 615)
 (function() {
-    // boton para mostrar el modal de agregar tarea 
+    // boton para mostrar la Ventana Modal para agregar una tarea 
     const nuevaTareaBtn = document.querySelector('#agregar-tarea');
     nuevaTareaBtn.addEventListener('click', mostrarFormulario);
 
     function mostrarFormulario(){
-        console.log("desde mostrarFormulario");
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label for="tarea">Tarea</label>
+                    <input 
+                        type="text"
+                        id="tarea"
+                        placeholder="Añadir Tarea al Proyecto Actual"
+                        name="tarea"
+                    >
+                </div>
+                <div class="opciones">
+                    <input 
+                        type="submit"
+                        class="submit-nueva-tarea"
+                        value="Añadir Tarea"
+                    >
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
+        document.querySelector('body').appendChild(modal);
+        console.log(modal);
     }
 })();
