0522ef6dc2f656af4a798016bc0701a42b29e088
lionel-prats <lionel.prats.c@gmail.com>
2023-06-27 00:27:36 -0300

    seccion 68 - video 613

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -90,10 +90,9 @@ class ActiveRecord {
         // Insertar en la base de datos
         $query = " INSERT INTO " . static::$tabla . " ( ";
         $query .= join(', ', array_keys($atributos));
-        $query .= " ) VALUES (' "; 
+        $query .= " ) VALUES ('"; 
         $query .= join("', '", array_values($atributos));
         $query .= " ') ";
-
         // Resultado de la consulta
         $resultado = self::$db->query($query);
 
