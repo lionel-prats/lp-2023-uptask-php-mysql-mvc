e22306c61f6fc57a24fb34aa864d8e89191e034d
lionel-prats <lionel.prats.c@gmail.com>
2023-08-13 12:25:48 -0300

    seccion 74 - video 651

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,4 +1,10 @@
-<div class="barra">
+<div class="barra-mobile"> <!-- barra dispositivos moviles  -->
+    <h1>UpTask</h1>
+    <div class="menu">
+        <img src="build/img/menu.svg" alt="imagen menu" id="mobile-menu">
+    </div>
+</div>
+<div class="barra"> <!-- barra a partir de tablet -->
     <p>Hola <span><?= $_SESSION["nombre"]?></span></p>
     <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
 </div>
